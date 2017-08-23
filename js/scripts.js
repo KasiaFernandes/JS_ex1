e28ce187c931982ecad010f1fd5c4889511cd97e
Kasia Fernandes <fernandes.kasia@gmail.com>
2017-08-23 15:59:41 +0200

    Add a new branch, ex.3 week8

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 // scripts.js
 
-var name = prompt('Enter your name');
-alert('Hello, ' + name);
-console.log('Hello, ' + name);
+var name = prompt('Enter your name');//This is my first js comment
+/*alert('Hello, ' + name);*/
+console.log('Hello, ' + name);//was this exercise really just about 
+//inserting comments?

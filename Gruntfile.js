addf76bc4ecc7ba7618d31047d2e63f40b3163f7
Kasia Fernandes <fernandes.kasia@gmail.com>
2017-08-23 16:32:43 +0200

    Gruntfile corrected

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,38 +2,37 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-      sass: {
+    sass: {
       options: {
         sourceMap: true
       },
-          
       dist: {
         files: {
           'css/style.css': 'JS_ex1.sass'
         }
       }
     },
-    
+
+    jshint: {
+      all: ['js/*.js']
+    },
+      
     watch: {
-    scripts: {
+      scripts: {
         files: ['*.sass'],
         tasks: ['sass'],
         options: {
             spawn: false,
-        },
-    }
-},
-
-    jshint: {
-      all: ['js/*.js']
-    }
+         },
+       }   
+     },
   });
 
   // Load the plugins tasks
   grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');    
   grunt.loadNpmTasks('grunt-contrib-jshint');
-    
+  grunt.loadNpmTasks('grunt-contrib-watch');    
+
   // Default task(s).
-  grunt.registerTask('default', ['sass', 'watch', 'jshint']);
+  grunt.registerTask('default', ['sass', 'jshint', 'watch']);
 };
